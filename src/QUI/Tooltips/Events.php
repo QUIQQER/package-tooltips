88f57d846693203e1d9b7b0eb6fdc45702076d12
Henning Leutz <leutz@pcsg.de>
2015-08-11 10:47:40 +0200

    html5 tooltips

---
+++ src/QUI/Tooltips/Events.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file contains \QUI\Tooltips\Events
+ */
+namespace QUI\Tooltips;
+
+/**
+ * Event handling
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+class Events
+{
+    static function onTemplateGetHeader($Template)
+    {
+        $Template->extendHeaderWithCSSFile(URL_OPT_DIR .'quiqqer/tooltips/bin/html5tooltips.css');
+        $Template->extendHeaderWithCSSFile(URL_OPT_DIR .'quiqqer/tooltips/bin/html5tooltips.animation.css');
+
+        $Template->extendHeader(
+            '<script type="text/javascript">
+            /* <![CDATA[ */
+                require.config({
+                    paths : {
+                        html5tooltips : URL_OPT_DIR +"quiqqer/tooltips/bin/html5tooltips.1.4.min"
+                    }
+                });
+
+                require([\'html5tooltips\'], function(html5tooltips) {
+                    html5tooltips.autoinit();
+                });
+            /* ]]> */
+            </script>'
+        );
+    }
+}
