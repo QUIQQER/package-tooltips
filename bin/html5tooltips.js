dd5f2203377e9546a6ddfd9fc16c30de53746178
Henning Leutz <leutz@pcsg.de>
2021-07-13 09:12:20 +0200

    refactor: use of quiqqer assets

--- bin/html5tooltips.js
+++ bin/html5tooltips.js
@@ -1,6 +1,6 @@
 define('package/quiqqer/tooltips/bin/html5tooltips', [
 
-    'package/bin/html5tooltipsjs/html5tooltips'
+    'package/bin/quiqqer-asset/html5tooltipsjs/html5tooltipsjs/html5tooltips'
 
 ], function (html5tooltips) {
     return html5tooltips;
